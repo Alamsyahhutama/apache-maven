da2202fa7dfbf9f5b0220eaf215ffa371bfc8599
Alamsyah H. <hutamaalamsyah@gmail.com>
2024-07-14 20:08:03 +0700

    apache-maven

---
+++ belajar-apache-maven-app/src/main/java/kang/alamsyah/hutama/maven/App.java
@@ -0,0 +1,19 @@
+package kang.alamsyah.hutama.maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        Person person = new Person("Alamsyah Hutama");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
